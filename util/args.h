d26898488560e379199df74a5ef81f7f5d1ff122
Brendan Keith Mark Watling <bkmwatling@protonmail.com>
2023-10-30 23:31:25 +0000

    Implement custom arguments for argument parser and fixed formatting for argument parser usage

--- util/args.h
+++ util/args.h
@@ -3,35 +3,72 @@
 
 #include <stdio.h>
 
+/**
+ * Prototype for custom usage printing function.
+ *
+ * @param[in] stream the I/O stream to print to
+ * @param[in] program the name of the program as it is executed
+ */
 typedef void StcArgsUsage(FILE *stream, const char *program);
 
 typedef enum {
-    STC_ARG_STR,  /*<< string argument                                        */
-    STC_ARG_BOOL, /*<< boolean argument                                       */
+    STC_CR_SUCCESS,         /*<< conversion was successful                    */
+    STC_CR_FAILURE,         /*<< conversion failed but not reported           */
+    STC_CR_FAILURE_HANDLED, /*<< conversion failed and function reported it   */
+} StcArgConvertResult;
+
+/**
+ * Prototype for conversion function of an argument to a custom type. Note that
+ * the string argument will never be NULL and thus does not have to be handled
+ * since NULL default value means no default. The output pointer will never be
+ * NULL and thus does not have to be checked.
+ *
+ *
+ * @param[in] arg the string argument
+ * @param[out] out the memory address where to store the custom converted value
+ *
+ * @return STC_CR_SUCCESS on successful converions, else STC_CR_FAILURE on
+ *         failure, but function did not report it (in which case the parser
+ *         will report it), else STC_CR_FAILURE_HANDLED on failure, but the
+ *         function did report (thus the parser will not report anything but
+ *         only print usage)
+ */
+typedef StcArgConvertResult StcArgConvert(const char *arg, void *out);
+
+typedef enum {
+    STC_ARG_STR,    /*<< string argument                                      */
+    STC_ARG_BOOL,   /*<< boolean argument                                     */
+    STC_ARG_CUSTOM, /*<< custom argument to be paired with convert function   */
 } StcArgType;
 
 typedef struct {
-    StcArgType type;        /*<< type of the argument                         */
-    char      *shortopt;    /*<< short name string (must start with '-' if
-                                 longopt does)                                */
-    char      *longopt;     /*<< long name string (must start with '-' if
-                                 shortopt does)                               */
-    char      *valname;     /*<< optional name of the value to show in usage  */
-    char      *description; /*<< optional description of the argument         */
-    void      *def;         /*<< optional default value of the argument       */
-    void      *out;         /*<< optional pointer to store argument value     */
+    StcArgType     type;        /*<< type of the argument                     */
+    const char    *shortopt;    /*<< short name string (must start with '-' if
+                                     longopt does)                            */
+    const char    *longopt;     /*<< long name string (must start with '-' if
+                                     shortopt does)                           */
+    void          *out;         /*<< optional pointer to store argument value */
+    const char    *valname;     /*<< optional name of value to show in usage  */
+    const char    *description; /*<< optional description of the argument     */
+    const void    *def;         /*<< optional default value of the argument
+                                     (must be the string of the default value
+                                     for the convert function for custom
+                                     arguments)   */
+    StcArgConvert *convert; /*<< optional function to convert custom argument */
+
 } StcArg;
 
-#define STC_ARG_NULL                          \
-    {                                         \
-        0, NULL, NULL, NULL, NULL, NULL, NULL \
+#define STC_ARG_NULL                                \
+    {                                               \
+        0, NULL, NULL, NULL, NULL, NULL, NULL, NULL \
     }
 
-#define STC_ARG_IS_POSITIONAL(arg)                   \
-    (((arg).shortopt && (arg).shortopt[0] != '-') || \
-     ((arg).longopt && (arg).longopt[0] != '-'))
-#define STC_ARG_IS_STR(argtype)  ((argtype) == STC_ARG_STR)
-#define STC_ARG_IS_BOOL(argtype) ((argtype) == STC_ARG_BOOL)
+#define STC_ARG_IS_POSITIONAL(arg)                     \
+    (((arg)->shortopt && (arg)->shortopt[0] != '-') || \
+     ((arg)->longopt && (arg)->longopt[0] != '-'))
+#define STC_ARG_IS_STR(argtype)    ((argtype) == STC_ARG_STR)
+#define STC_ARG_IS_BOOL(argtype)   ((argtype) == STC_ARG_BOOL)
+#define STC_ARG_IS_CUSTOM(argtype) ((argtype) == STC_ARG_CUSTOM)
 
 /**
  * Parses the command-line arguments using the given array of argument
@@ -68,6 +105,15 @@ void stc_args_parse_exact(int           argc,
                           int           args_len,
                           StcArgsUsage *usage);
 
+/**
+ * Prints the usage information of the program based on the given argument
+ * specifications.
+ *
+ * @param[in] stream the I/O stream to print to
+ * @param[in] program the name of the program as it is executed
+ * @param[in] args the array of argument specifications
+ * @param[in] args_len the length the array of argument specifications
+ */
 void stc_args_usage(FILE         *stream,
                     const char   *program,
                     const StcArg *args,
