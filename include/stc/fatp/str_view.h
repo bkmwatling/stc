bac0781487a4d023f01033c2876ec82a3ad7651a
Brendan Keith Mark Watling <bkmwatling@protonmail.com>
2025-02-17 02:01:26 +0200

    chore(str_view): Rename string view chop functions to split

--- include/stc/fatp/str_view.h
+++ include/stc/fatp/str_view.h
@@ -29,24 +29,24 @@ typedef StcStrView StrView;
 #    define sv_subview_from  stc_sv_subview_from
 #    define sv_subview_until stc_sv_subview_until
 
-#    define sv_trim              stc_sv_trim
-#    define sv_trim_left         stc_sv_trim_left
-#    define sv_trim_right        stc_sv_trim_right
-#    define sv_take_left_while   stc_sv_take_left_while
-#    define sv_chop_left_while   stc_sv_chop_left_while
-#    define sv_chop_by_delim     stc_sv_chop_by_delim
-#    define sv_chop_by_sv        stc_sv_chop_by_sv
-#    define sv_try_chop_by_delim stc_sv_try_chop_by_delim
-#    define sv_chop_left         stc_sv_chop_left
-#    define sv_chop_right        stc_sv_chop_right
-#    define sv_index_of          stc_sv_index_of
-#    define sv_cmp               stc_sv_cmp
-#    define sv_eq                stc_sv_eq
-#    define sv_eq_ignorecase     stc_sv_eq_ignorecase
-#    define sv_starts_with       stc_sv_starts_with
-#    define sv_ends_with         stc_sv_ends_with
-#    define sv_to_int            stc_sv_to_int
-#    define sv_chop_int          stc_sv_chop_int
+#    define sv_trim               stc_sv_trim
+#    define sv_trim_left          stc_sv_trim_left
+#    define sv_trim_right         stc_sv_trim_right
+#    define sv_take_left_while    stc_sv_take_left_while
+#    define sv_split_left_while   stc_sv_split_left_while
+#    define sv_split_by_delim     stc_sv_split_by_delim
+#    define sv_split_by_sv        stc_sv_split_by_sv
+#    define sv_try_split_by_delim stc_sv_try_split_by_delim
+#    define sv_split_left         stc_sv_split_left
+#    define sv_split_right        stc_sv_split_right
+#    define sv_index_of           stc_sv_index_of
+#    define sv_cmp                stc_sv_cmp
+#    define sv_eq                 stc_sv_eq
+#    define sv_eq_ignorecase      stc_sv_eq_ignorecase
+#    define sv_starts_with        stc_sv_starts_with
+#    define sv_ends_with          stc_sv_ends_with
+#    define sv_to_int             stc_sv_to_int
+#    define sv_split_int          stc_sv_split_int
 #endif /* STC_SV_ENABLE_SHORT_NAMES */
 
 #if defined(STC_ENABLE_SHORT_NAMES) || defined(STC_STR_ENABLE_SHORT_NAMES)
@@ -325,7 +325,7 @@ StcStrView stc_sv_trim_right(StcStrView self);
 StcStrView stc_sv_take_left_while(StcStrView self, int (*predicate)(char));
 
 /**
- * Chop the string view self from the left and return the left side (prefix) of
+ * Split the string view self from the left and return the left side (prefix) of
  * self such that the returned string view's characters all match the predicate.
  *
  * The returned string view is a maximal prefix of the self with all characters
@@ -335,17 +335,17 @@ StcStrView stc_sv_take_left_while(StcStrView self, int (*predicate)(char));
  * the string it used to view. In this case the returned string view will be a
  * copy of what self was.
  *
- * @param[in,out] self      the pointer to the string view to chop
+ * @param[in,out] self      the pointer to the string view to split
  * @param[in]     predicate the predicate function to test the characters of the
  *                          string view
  *
  * @return a string view of the maximal prefix of self with all characters
  *         matching the predicate
  */
-StcStrView stc_sv_chop_left_while(StcStrView *self, int (*predicate)(char));
+StcStrView stc_sv_split_left_while(StcStrView *self, int (*predicate)(char));
 
 /**
- * Chop the string view self at the delimeter and return a string view of the
+ * Split the string view self at the delimeter and return a string view of the
  * self from the left of the delimeter and update self to view the string from
  * the right of the delimeter.
  *
@@ -353,15 +353,15 @@ StcStrView stc_sv_chop_left_while(StcStrView *self, int (*predicate)(char));
  * point to the end of the string is used to view. In this case the returned
  * string view will be a copy of what self was.
  *
- * @param[in,out] self  the pointer to the string view to chop
- * @param[in]     delim the character delimeter to chop the string view at
+ * @param[in,out] self  the pointer to the string view to split
+ * @param[in]     delim the character delimeter to split the string view at
  *
  * @return a string view of the string to the left of the delimeter
  */
-StcStrView stc_sv_chop_by_delim(StcStrView *self, char delim);
+StcStrView stc_sv_split_by_delim(StcStrView *self, char delim);
 
 /**
- * Chop the string view self at the delimeter and return a string view of the
+ * Split the string view self at the delimeter and return a string view of the
  * self from the left of the delimeter and update self to view the string from
  * the right of the delimeter.
  *
@@ -369,15 +369,15 @@ StcStrView stc_sv_chop_by_delim(StcStrView *self, char delim);
  * point to the end of the string is used to view. In this case the returned
  * string view will be a copy of what self was.
  *
- * @param[in,out] self  the pointer to the string view to chop
- * @param[in]     delim the string view delimeter to chop the string view at
+ * @param[in,out] self  the pointer to the string view to split
+ * @param[in]     delim the string view delimeter to split the string view at
  *
  * @return a string view of the string to the left of the delimeter
  */
-StcStrView stc_sv_chop_by_sv(StcStrView *self, StcStrView delim);
+StcStrView stc_sv_split_by_sv(StcStrView *self, StcStrView delim);
 
 /**
- * Try to chop the string view self at the delimeter and return whether or not
+ * Try to split the string view self at the delimeter and return whether or not
  * the delimeter was found in self.
  *
  * If the delimeter is found in self, then a string view of self from the left
@@ -390,39 +390,39 @@ StcStrView stc_sv_chop_by_sv(StcStrView *self, StcStrView delim);
  * attempted, and so it can be set to NULL if you don't care about string on the
  * left side of the delimeter.
  *
- * @param[in,out] self  the pointer to the string view to chop
- * @param[in]     delim the string view delimeter to chop the string view at
+ * @param[in,out] self  the pointer to the string view to split
+ * @param[in]     delim the string view delimeter to split the string view at
  * @param[out]    left  the pointer to the string view to store the string on
  *                      the left of the delimiter if found and is not NULL
  *
  * @return whether the delimeter was found and thus if the string views where
  *         modified
  */
-int stc_sv_try_chop_by_delim(StcStrView *self, char delim, StcStrView *left);
+int stc_sv_try_split_by_delim(StcStrView *self, char delim, StcStrView *left);
 
 /**
- * Chop off the first n characters from the string view and return a string view
- * to the chopped off characters whilst updating the given string view to start
- * after the last chopped off character.
+ * Split off the first n characters from the string view and return a string
+ * view to the split off characters whilst updating the given string view to
+ * start after the last split off character.
  *
- * @param[in,out] self the pointer to the string view to chop
- * @param[in]     n    the number of characters to chop from the left
+ * @param[in,out] self the pointer to the string view to split
+ * @param[in]     n    the number of characters to split from the left
  *
- * @return a string view to the characters that were chopped off
+ * @return a string view to the characters that were split off
  */
-StcStrView stc_sv_chop_left(StcStrView *self, size_t n);
+StcStrView stc_sv_split_left(StcStrView *self, size_t n);
 
 /**
- * Chop off the last n characters from the string view and return a string view
- * to the chopped off characters whilst updating the given string view to end
- * before the first chopped off character.
+ * Split off the last n characters from the string view and return a string view
+ * to the split off characters whilst updating the given string view to end
+ * before the first split off character.
  *
- * @param[in,out] self the pointer to the string view to chop
- * @param[in]     n    the number of characters to chop from the right
+ * @param[in,out] self the pointer to the string view to split
+ * @param[in]     n    the number of characters to split from the right
  *
- * @return a string view to the characters that were chopped off
+ * @return a string view to the characters that were split off
  */
-StcStrView stc_sv_chop_right(StcStrView *self, size_t n);
+StcStrView stc_sv_split_right(StcStrView *self, size_t n);
 
 /**
  * Find the index of the character c in the string view if it is contained in
@@ -432,7 +432,8 @@ StcStrView stc_sv_chop_right(StcStrView *self, size_t n);
  * @param[in]  c    the character to find the index of
  * @param[out] idx  the pointer to save the index of c in only if found
  *
- * @return whether the character was found in the string view
+ * @return a non-zero value if the character was found in the string view;
+ *         else 0 if the character was not found in the string view
  */
 int stc_sv_index_of(StcStrView self, char c, size_t *idx);
 
@@ -517,6 +518,6 @@ size_t stc_sv_to_int(StcStrView self);
  *
  * @return the unsigned integer parsed from the string view
  */
-size_t stc_sv_chop_int(StcStrView *self);
+size_t stc_sv_split_int(StcStrView *self);
 
 #endif /* STC_STR_VIEW_H */
